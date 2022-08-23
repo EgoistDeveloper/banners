e4652a5c1c281b790bcf5a11f0a7419ecc2f8c78
Egoist <epsilonelittr@gmail.com>
2022-08-23 07:56:20 +0300

    Add custom watermark text option

--- api/_lib/types.ts
+++ api/_lib/types.ts
@@ -1,20 +1,21 @@
-export type FileType = 'png' | 'jpeg';
-export type Theme = 'light' | 'dark';
-export type Style = 'style_1' | 'style_2';
+export type FileType = "png" | "jpeg";
+export type Theme = "light" | "dark";
+export type Style = "style_1" | "style_2";
 
 export interface ParsedRequest {
-    fileType: FileType;
-    text: string;
-    theme: Theme;
-    style: Style;
-    showWatermark: boolean;
-    packageManager: string;
-    packageName: string;
-    description: string;
-    pattern: string;
-    md: boolean;
-    fontSize: string;
-    images: string[];
-    widths: string[];
-    heights: string[];
+  fileType: FileType;
+  text: string;
+  theme: Theme;
+  style: Style;
+  showWatermark: boolean;
+  watermarkText: string;
+  packageManager: string;
+  packageName: string;
+  description: string;
+  pattern: string;
+  md: boolean;
+  fontSize: string;
+  images: string[];
+  widths: string[];
+  heights: string[];
 }
